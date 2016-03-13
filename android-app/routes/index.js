f9c529f5bab4eca8759df94b14747ba75364526f
Nam <leanhnam2203@gmail.com>
2016-03-13 10:35:23 +0700

    fix account API

--- android-app/routes/index.js
+++ android-app/routes/index.js
@@ -2,34 +2,58 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
-var Server = mongo.Server,
+var mongoClient = mongo.MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/android-user';
+/*var Server = mongo.Server,
 	Db = mongo.Db,
 	BSon = mongo.BSonPure;
 var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('android-user', server);
-db.open(function(err, db) {
+db = new Db('android-user', server);*/
+
+
+router.post('/login', function(req, res) {
+    mongoClient.connect(url, function(err, db) {
+    console.log("Connecting.....")
     if (!err) {
         console.log("Connected to database");
-        db.collection('user', {strict: true}, function(err, collection) {
-            if (err) {
-             	console.log('Wrong data!')   
-            }
-        });
     }
-}); 
-router.post('/login', function(req, res) {
-	var user_json = db.collection('account').find(req.body.name).buf.toJSON(function(err,message){
-		if(err) console.log('can not access data');
-	})
-	console.log(user_json.name);
+    var account = db.collection('account');
+    var username = req.body.username;
+    account.find(req.body, function(err, count) {
+        // body...
+        res.send(count.length);
+    });
+    db.close();
+    }); 
+    
 });
- router.post('/register',function(req,res){
- 	MongoClient.connect(URL, function(err,db){
- 		if (err) {
- 			console.log(err);
- 			return;
- 		}
- 		var collection = db.collection('account')
- 	});
- });
+router.post('/register',function(req,res){
+ 	mongoClient.connect(url, function(err, db) {
+    console.log("Connecting.....")
+    if (!err) {
+        console.log("Connected to database");
+    }
+    var account = db.collection('account');
+    account.insert({
+        name:req.body.username,
+        password:req.body.password,
+        email:req.body.email
+    });
+    res.send("success");
+    db.close();
+    }); 
+}); 
+router.post('/resetpassword',function(req,res){
+    mongoClient.connect(url, function(err, db) {
+    console.log("Connecting.....")
+    if (!err) {
+        console.log("Connected to database");
+    }
+    var account = db.collection('account');
+    account.findOneAndUpdate({name:req.body.name},{password:'1234566'});
+    res.send("success");
+    db.close();
+    }); 
+}); 
 module.exports = router;
